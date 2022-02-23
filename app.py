1b78ec3f03c0b9683e7bbab49bd73081a5dfc899
Omar Cardoso <omarmello18@gmail.com>
2022-02-23 17:29:04 -0300

    Adicionado app.py onde tem o exemplo de REST API para manipular dados referete a um desenvolvedor e suas habilidades.

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/<int:id>')
+def pessoa(id):
+    return jsonify({'id':id, 'nome':'Rafael', 'profissao':'Desenvolvedor'})
+
+#@app.route('/soma/<int:valor1>/<int:valor2>/<int:valor3>')
+#def soma(valor1, valor2,valor3):
+#    return jsonify({'soma': valor1 + valor2 + valor3})
+
+
+def josonify(param):
+    pass
+
+
+#@app.route('/soma/<int:valor1>/<int:valor2>/')
+#def soma(valor1,valor2):
+#    return jsonify({'soma':valor1 + valor2})
+
+#    dados = request.data
+#    dados = json.loads(request.data)
+#    total = sum(dados['valores'])
+#    return jsonify({'soma':total})
+
+#@app.route('/soma', methods=['POST'])
+#def soma():
+#    dados = request.data
+#   print(dados)
+ #   return ('soma')
+
+@app.route('/soma', methods=['POST', 'GET'])
+def soma():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        total = sum(dados['valores'])
+    elif request.method == 'GET':
+        total = 10 + 10
+    return jsonify({'soma':total})
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
